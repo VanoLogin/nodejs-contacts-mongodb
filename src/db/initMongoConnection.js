7f4dc72efa7a88862e3c2a1df7dc071df80c389e
Login_vano1234 <116017080+VanoLogin@users.noreply.github.com>
2024-07-17 15:34:34 +0300

    update_to_deploy

--- src/db/initMongoConnection.js
+++ src/db/initMongoConnection.js
@@ -1,13 +1,21 @@
 import mongoose from 'mongoose';
-
-const DB_URI = process.env.DB_URI;
+import 'dotenv/config';
 
 async function initMongoConnection() {
   try {
-    await mongoose.connect(DB_URI, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-    });
+    const user = process.env.MONGODB_USER;
+    const pwd = process.env.MONGODB_PASSWORD;
+    const url = process.env.MONGODB_URL;
+    const db = process.env.MONGODB_DB;
+
+    console.log('MONGODB_USER:', user);
+    console.log('MONGODB_PASSWORD:', pwd);
+    console.log('MONGODB_URL:', url);
+    console.log('MONGODB_DB:', db);
+
+    const connectionString = `mongodb+srv://${user}:${pwd}@${url}/${db}?retryWrites=true&w=majority&appName=Contacts`;
+
+    await mongoose.connect(connectionString);
 
     console.log('Mongo connection successfully established!');
   } catch (error) {
